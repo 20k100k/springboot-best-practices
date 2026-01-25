a1e4144417f66dae04d4b843ea5325f8d6fd2a91
xperia <xperia@ZBvkn.>
2026-01-25 09:36:31 +0100

    Add test

---
+++ src/test/java/vkn/xperia/sb/bp/SpringbootBestPracticesApplicationTest.java
@@ -0,0 +1,16 @@
+package vkn.xperia.sb.bp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringbootBestPracticesApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void test1(){
+        assertEquals(1,1);
+    }
+}
